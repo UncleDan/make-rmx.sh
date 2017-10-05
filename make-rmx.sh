7ca8c16d17f93972a374838bd76320a40e0d8360
Daniele Lolli <uncledan@uncledan.it>
2017-10-05 17:03:52 +0200

    sort and divide software plus minimal fix

--- make-rmx.sh
+++ make-rmx.sh
@@ -7,13 +7,117 @@ echo "|_,_|_____|___|___|_|_|_| |___|__|__|_|_|_|__|__|  |_____|___|_|___| |_|_|
 echo "                                                                              "
 cd ~
 
-echo "Making sure everything is up to date..."
-sudo apt-get --assume-yes install openssh-server
+echo "Finishing suspended configurations (if any)..."
+sudo dpkg --configure -a
 echo "Done."
 echo ""
 
+echo "Making sure everything is up to date..."
+sudo apt-get --assume-yes clean
+sudo apt-get --assume-yes update
+sudo apt-get --assume-yes upgrade
+echo "Done."
+echo ""
+
+echo "INSTALLING  B A S I C  PACKAGES..."
+echo
+
+echo "Installing 7zip..."
+sudo apt-get --assume-yes install p7zip-full p7zip-rar
+echo "Done."
+echo
+
+echo "Installing Gimp..."
+sudo apt-get --assume-yes install gimp
+echo "Done."
+echo
+
+echo "Installing Google Chrome from PPA..."
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+[ -f /etc/apt/sources.list.d/google-chrome.list ] && sudo rm /etc/apt/sources.list.d/google-chrome.list
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get --assume-yes install google-chrome-stable
+echo "Done."
+echo
+
 echo "Installing OpenSSH Server..."
 sudo apt-get --assume-yes install openssh-server
+echo "Done."
+echo
+
+echo "Installing pCloud Client 3.1.1 from deb package..."
+wget https://github.com/UncleDan/files/raw/master/pCloud_Linux_amd64_3.1.1.deb #Url in pClud site masked: using mirror
+sudo dpkg -i pCloud_Linux_amd64_3.1.1.deb
+rm pCloud_Linux_amd64_3.1.1.deb
+echo "S" | sudo apt-get -f install
+echo "Done."
+echo
+
+echo "Installing Teamviewer 11 from deb package..."
+wget http://download.teamviewer.com/download/version_11x/teamviewer_i386.deb
+sudo dpkg -i teamviewer_i386.deb
+rm teamviewer_i386.deb
+echo "S" | sudo apt-get -f install
+echo "Done."
+echo
+
+echo "Installing VLC (Video Lan Client)..."
+sudo apt-get --assume-yes install vlc
+echo "Done."
+echo
+
+echo "INSTALLING  D E V E L O P E R  PACKAGES..."
+echo
+
+echo "Installing Angry IP Scanner 3.5.1 from deb package..."
+sudo apt-get --assume-yes install openjdk-8-jre
+echo "S" | sudo apt-get -f install
+wget https://github.com/angryip/ipscan/releases/download/3.5.1/ipscan_3.5.1_amd64.deb 
+sudo dpkg -i ipscan_3.5.1_amd64.deb
+rm ipscan_3.5.1_amd64.deb
+echo "S" | sudo apt-get -f install
+echo "Done."
+echo
+
+echo "Installing Blue Griffon 2.3.1 from deb package..."
+wget http://bluegriffon.org/freshmeat/2.3.1/bluegriffon_2.3.1-1ubuntu1_amd64.deb
+sudo dpkg -i bluegriffon_2.3.1-1ubuntu1_amd64.deb
+rm bluegriffon_2.3.1-1ubuntu1_amd64.deb
+echo "S" | sudo apt-get -f install
+echo "Done."
+echo
+
+echo "Installing Filezilla..."
+sudo apt-get --assume-yes install filezilla
+echo "Done."
+echo
+
+echo "Installing FreeFileSync 8.6 from deb package..."
+wget http://archive.getdeb.net/ubuntu/pool/apps/f/freefilesync/freefilesync_8.6-1~getdeb1_amd64.deb
+sudo dpkg -i freefilesync_8.6-1~getdeb1_amd64.deb
+rm freefilesync_8.6-1~getdeb1_amd64.deb
+echo "S" | sudo apt-get -f install
+echo "Done."
+echo
+
+echo "Installing Git..."
+sudo apt-get --assume-yes install git
+echo "Done."
+echo
+
+echo "Installing Gparted..."
+sudo apt-get --assume-yes install gparted
+echo "Done."
+echo
+
+echo "Installing KeePassX 0.4.3 from deb package (I need to work with KDB v1.x)..."
+wget http://security.ubuntu.com/ubuntu/pool/universe/k/keepassx/keepassx_0.4.3+dfsg-0.1ubuntu1.14.04.1_amd64.deb
+sudo dpkg -i keepassx_0.4.3+dfsg-0.1ubuntu1.14.04.1_amd64.deb
+rm keepassx_0.4.3+dfsg-0.1ubuntu1.14.04.1_amd64.deb
+echo "S" | sudo apt-get -f install
+echo "Locking updates (newer versions do not support KDB v1.x)..."
+echo "keepassx hold" | sudo dpkg --set-selections
 echo "Done."
 echo
 
@@ -28,53 +132,10 @@ sudo apt-get --assume-yes install apache2 mysql-server mysql-client php7.0 libap
 echo "Done."
 echo
 
-echo "Installing Ruby, Gems and Jekyll..."
-sudo apt-get --assume-yes install ruby rubygems ruby-dev zlibc zlib1g zlib1g-dev
-sudo gem install jekyll bundler github-pages
-echo "Testing Jekyll ( http://localhost/ )..."
-jekyll new Jekyll
-cd Jekyll
-sudo jekyll build -d /var/www/html/
-cd ..
-echo "Done."
-echo
-
-echo "Installing Git..."
-sudo apt-get --assume-yes install git
-echo "Done."
-echo
-
-echo "Installing Google Chrome from PPA..."
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-[ -f /etc/apt/sources.list.d/google-chrome.list ] && sudo rm /etc/apt/sources.list.d/google-chrome.list
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+echo "Installing Notepadqq from PPA (a wonderful Notepad++ clone)..."
+echo | sudo add-apt-repository ppa:notepadqq-team/notepadqq
 sudo apt-get update
-sudo apt-get --assume-yes install google-chrome-stable
-echo "Done."
-echo
-
-echo "Installing antivirus software..."
-sudo apt-get --assume-yes install clamav clamtk chkrootkit rkhunter  
-echo "Done."
-echo
-
-echo "Installing Filezilla..."
-sudo apt-get --assume-yes install filezilla
-echo "Done."
-echo
-
-echo "Installing Gimp..."
-sudo apt-get --assume-yes install gimp
-echo "Done."
-echo
-
-echo "Installing Gparted..."
-sudo apt-get --assume-yes install gparted
-echo "Done."
-echo
-
-echo "Installing 7zip..."
-sudo apt-get --assume-yes install p7zip-full
+sudo apt-get --assume-yes install notepadqq
 echo "Done."
 echo
 
@@ -88,67 +149,14 @@ sudo apt-get --assume-yes install remmina
 echo "Done."
 echo
 
-echo "Installing VLC (Video Lan Client)..."
-sudo apt-get --assume-yes install vlc
-echo "Done."
-echo
-
-echo "Installing Notepadqq from PPA (a wonderful Notepad++ clone)..."
-echo | sudo add-apt-repository ppa:notepadqq-team/notepadqq
-sudo apt-get update
-sudo apt-get --assume-yes install notepadqq
-echo "Done."
-echo
-
-echo "Installing Angry IP Scanner 3.5.1 from deb package..."
-sudo apt-get install openjdk-8-jre
-sudo apt-get -f install
-wget https://github.com/angryip/ipscan/releases/download/3.5.1/ipscan_3.5.1_amd64.deb 
-sudo dpkg -i ipscan_3.5.1_amd64.deb
-rm ipscan_3.5.1_amd64.deb
-sudo apt-get -f install
-echo "Done."
-echo
-
-echo "Installing FreeFileSync 8.6 from deb package..."
-wget http://archive.getdeb.net/ubuntu/pool/apps/f/freefilesync/freefilesync_8.6-1~getdeb1_amd64.deb
-sudo dpkg -i freefilesync_8.6-1~getdeb1_amd64.deb
-rm freefilesync_8.6-1~getdeb1_amd64.deb
-sudo apt-get -f install
-echo "Done."
-echo
-
-echo "Installing KeePassXFreeFileSync 0.4.3 from deb package (I need to work with KDB v1.x)..."
-wget http://security.ubuntu.com/ubuntu/pool/universe/k/keepassx/keepassx_0.4.3+dfsg-0.1ubuntu1.14.04.1_amd64.deb
-sudo dpkg -i keepassx_0.4.3+dfsg-0.1ubuntu1.14.04.1_amd64.deb
-rm keepassx_0.4.3+dfsg-0.1ubuntu1.14.04.1_amd64.deb
-echo "S" | sudo apt-get -f install
-echo "Locking updates (newer versions do not support KDB v1.x)..."
-echo "keepassx hold" | sudo dpkg --set-selections
-echo "Done."
-echo
-
-echo "Installing pCloud Client 3.1.1 from deb package..."
-wget https://github.com/UncleDan/files/raw/master/pCloud_Linux_amd64_3.1.1.deb #Url in pClud site masked: using mirror
-sudo dpkg -i pCloud_Linux_amd64_3.1.1.deb
-rm pCloud_Linux_amd64_3.1.1.deb
-echo "S" | sudo apt-get -f install
-echo "Done."
-echo
-
-echo "Installing Pinguy Builder 4.3.8 (beta) from deb package..."
-wget https://downloads.sourceforge.net/project/pinguy-os/ISO_Builder/pinguybuilder_4.3-8_all-beta.deb
-sudo dpkg -i pinguybuilder_4.3-8_all-beta.deb
-rm pinguybuilder_4.3-8_all-beta.deb
-echo "S" | sudo apt-get -f install
-echo "Done."
-echo
-
-echo "Installing Teamviewer 11 from deb package..."
-wget http://download.teamviewer.com/download/version_11x/teamviewer_i386.deb
-sudo dpkg -i teamviewer_i386.deb
-rm teamviewer_i386.deb
-echo "S" | sudo apt-get -f install
+echo "Installing Ruby, Gems and Jekyll..."
+sudo apt-get --assume-yes install ruby rubygems ruby-dev zlibc zlib1g zlib1g-dev
+sudo gem install jekyll bundler github-pages
+echo "Testing Jekyll ( http://localhost/ )..."
+jekyll new Jekyll
+cd Jekyll
+sudo jekyll build -d /var/www/html/
+cd ..
 echo "Done."
 echo
 
@@ -160,6 +168,25 @@ echo "S" | sudo apt-get -f install
 echo "Done."
 echo
 
+echo "Installing antivirus software..."
+sudo apt-get --assume-yes install clamav clamtk chkrootkit rkhunter  
+echo "Done."
+echo
+
+echo "INSTALLING  P I N G U Y  B U I L D E R  PACKAGES..."
+echo
+
+echo "Installing Pinguy Builder 4.3.8 (beta) from deb package..."
+wget https://downloads.sourceforge.net/project/pinguy-os/ISO_Builder/pinguybuilder_4.3-8_all-beta.deb
+sudo dpkg -i pinguybuilder_4.3-8_all-beta.deb
+rm pinguybuilder_4.3-8_all-beta.deb
+echo "S" | sudo apt-get -f install
+echo "Done."
+echo
+
+echo "INSTALLING  V I R T U A L B O X  PACKAGES..."
+echo
+
 echo "Installing Virtualbox 5.1.28 from deb package..."
 wget http://download.virtualbox.org/virtualbox/5.1.28/virtualbox-5.1_5.1.28-117968~Ubuntu~xenial_amd64.deb
 sudo dpkg -i virtualbox-5.1_5.1.28-117968~Ubuntu~xenial_amd64.deb
@@ -168,17 +195,12 @@ echo "S" | sudo apt-get -f install
 echo "Done."
 echo
 
-echo "Installing Blue Griffon 2.3.1 from deb package..."
-wget http://bluegriffon.org/freshmeat/2.3.1/bluegriffon_2.3.1-1ubuntu1_amd64.deb
-sudo dpkg -i bluegriffon_2.3.1-1ubuntu1_amd64.deb
-rm bluegriffon_2.3.1-1ubuntu1_amd64.deb
-echo "S" | sudo apt-get -f install
-echo "Done."
+echo "INSTALLING  I N V O I C E X  PACKAGES..."
 echo
 
 echo "Installing Invoicex 1.8.9 from jar package..."
 sudo apt-get install openjdk-8-jre
-sudo apt-get -f install
+echo "S" | sudo apt-get -f install
 wget http://server.invoicex.it/download/setup/Invoicex_Setup_1.8.9_20161025_linux.jar 
 java -jar Invoicex_Setup_1.8.9_20161025_linux.jar
 rm Invoicex_Setup_1.8.9_20161025_linux.jar
@@ -195,5 +217,6 @@ echo ""
 # * conky
 # * docky
 # * samba
+# * DA AGGIUNGERE: Virtualbox Extension Pack (command line?)
 # * DA AGGIUNGERE: Android SDK
 # * DA AGGIUNGERE: Software tipo Speccy
